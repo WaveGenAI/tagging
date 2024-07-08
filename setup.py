795b3959a3e19ad7830022a6d7d910f7318bab0a
Jourdelune <jourdelune863@gmail.com>
2024-07-08 13:43:17 +0200

    Refactor package data path in setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
-    package_data={"mypkg": ["tagging/data/*.txt"]},
+    package_data={"mypkg": ["data/*.txt"]},
 )
